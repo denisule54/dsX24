e516cf7a6d468aeb5dddd39eab8b2217ff6da505
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-05-31 16:47:18 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+TOKEN="7fd05cb6ce15d49eef7fb7204eb0e3e4f7ace99ed8cb63b5d3" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+~/.buildkite-agent/bin/buildkite-agent start
