df493a3322291c3f18e4e68db6a812173628d312
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-06-06 14:43:25 +0700

    Update screennanoverus.sh

--- screennanoverus.sh
+++ screennanoverus.sh
@@ -2,6 +2,6 @@
 
 nohup sudo apt-get update
 nohup sudo apt install screen -y
-nohup https://github.com/denisule54/dsX24/raw/main/newnanoverus.sh
+nohup wget https://github.com/denisule54/dsX24/raw/main/newnanoverus.sh
 chmod +x newnanoverus.sh 
 screen -d -m ./newnanoverus.sh
